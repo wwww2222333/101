f8ba1c561be4ff9931f63d9d37b6b00fa88f10ef
wwww2222333 <91441049+wwww2222333@users.noreply.github.com>
2021-10-13 16:18:58 -0700

    Add files via upload

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// Yan Yang Lexin Xiong
+// We create Pseudocoding for calculate average salary
+// 10/14/2021
+
+
+//Creating a average-salary calculator:
+// Inlitizlize salary_counter and sum =0
+// set the loop
+  // Add each salary to the sum
+  // Increment the counter
+// Loop through each salary
+// compute average = sum/counter
+// Print the average of salary
